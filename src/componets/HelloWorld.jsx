a1a766034cae6ca67e4b30d26e3a17e54840b520
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-29 21:36:18 -0500

    Configuración inicial de proyecto React

---
+++ src/componets/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HelloWorld = () => {
+    <h1>Hola Mundo Cruel</h1>
+}
+
+export default HelloWorld
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './componets/HelloWorld'
+
+ReactDOM.render(
+    <HelloWorld />, document.getElementById(app)
+)
