de3b87e20b70ad02e39ecedaa66f022468ee7618
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-30 03:04:27 -0500

    Se agrega StoryBook para visualizar los componentes facilmente

---
+++ src/componets/HelloWorld.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+export default {
+  title: 'HelloWorld',
+  component: HelloWorld,
+};
+
+export const Hola = () => <div>Hola</div>;
