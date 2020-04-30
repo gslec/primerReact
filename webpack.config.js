25cd01dd0456b63633dba064d19da3fb06afa793
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-29 22:18:45 -0500

    Se instalo y configuro webpack correctamente en el proyecto

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HmtlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HmtlWebPackPlugin({
+         template: './public/index.html',
+         filename: './index.html'
+        })
+    ]
+}
