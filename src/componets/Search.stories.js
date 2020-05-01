9d30bcf69c0b4a8902a1b8a0d3784ac9adb3ec99
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-05-01 01:10:21 -0500

    Se crea componente Header y sus respectivos archivos .scss

---
+++ src/componets/Search.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Search from './Search';
+
+export default {
+  title: 'Search',
+  component: Search,
+};
+
+export const Buscador = () => <Search />;
---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+import Header from '../componets/Header';
+import Search from '../componets/Search';
+
+const App = () => (
+  <div className='App'>
+    <Header />
+    <Search />
+  </div>
+);
+export default App;
